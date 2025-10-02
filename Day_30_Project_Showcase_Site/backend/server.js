71eee1e0ade9aa3e5c98ee982ceaa3b20c7ba59b
SamarthSaketh <Samarthsaketh@outlook.com>
2025-10-02 15:54:46 +0530

    Changes Done

--- Day_30_Project_Showcase_Site/backend/server.js
+++ Day_30_Project_Showcase_Site/backend/server.js
@@ -15,7 +15,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/projects", projectRoutes);
 
-// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
