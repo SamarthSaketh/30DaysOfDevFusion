4be569793a0014b4a0a5dc38dbb834f421e75973
SamarthSaketh <Samarthsaketh@outlook.com>
2026-03-01 14:35:26 +0530

    add certificate PDFs to public/certs

--- Day_30_Project_Showcase_Site/backend/server.js
+++ Day_30_Project_Showcase_Site/backend/server.js
@@ -15,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/projects", projectRoutes);
 
+
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
